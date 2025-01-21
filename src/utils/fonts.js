fce901ada1658a4a675e5d0e51c3ca963ac5e757
anshuman619 <mishraanshuman619@gmail.com>
2025-01-21 11:00:57 +0530

    update on 21/1/25

--- src/utils/fonts.js
+++ src/utils/fonts.js
@@ -1,15 +1,18 @@
-import { Nunito, Playfair_Display, Raleway } from "next/font/google";
-export const raleway = Raleway({
-  weight: "variable",
-  subsets: ["latin"],
-});
+// import { Nunito, Playfair_Display, Raleway } from "next/font/google";
+export const raleway = ""
+// Raleway({
+//   weight: "variable",
+//   subsets: ["latin"],
+// });
 
-export const display = Playfair_Display({
-  weight: "variable",
-  subsets: ["latin"],
-});
+export const display = ""
+// Playfair_Display({
+//   weight: "variable",
+//   subsets: ["latin"],
+// });
 
-export const nunito = Nunito({
-  weight: "variable",
-  subsets: ["latin"],
-});
+export const nunito =""
+// Nunito({
+//   weight: "variable",
+//   subsets: ["latin"],
+// });
