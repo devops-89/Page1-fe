bad4a42dda791ca58e274c3ee9deb09fae1a232f
anshuman619 <mishraanshuman619@gmail.com>
2025-04-11 17:50:16 +0530

    merging from the dev-bharti branch

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,14 +1,6 @@
 import { configureStore,combineReducers } from "@reduxjs/toolkit";
 
-// import userReducer from "../reducers/user";
-// import toastReducer from "../reducers/toast";
-// import travellerInformationReducer from "../reducers/travellerInformation";
-// import flightInformationReducer from "../reducers/flightInformation";
-// import formPayloadReducer from "../reducers/formPayload";
-// import seatsInformationReducer from "../reducers/seatsInformation";
-// import roundInternationalSeatsInformationReducer from "../reducers/roundInternationalSeatsInformation";
-// import mealsInformationReducer from "../reducers/mealsInformation";
-// import baggagesInformationReducer from '../reducers/baggagesInformation'
+
 import flightReducer from "../reducers/flight";
 import userReducer from "../reducers/userReducer";
 import toastReducer from "../reducers/toastReducer";
@@ -23,7 +15,7 @@ export default configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }),
+    }), 
   reducer: rootReducer
  
 });
